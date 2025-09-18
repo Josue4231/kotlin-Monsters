b09aae794a71c0e2c8ca812900267d63a15b332a
josue <josuekialengela@gmail.com>
2025-09-18 09:23:46 +0200

    ini

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,9 @@ fun main() {
     println(changeCouleur("Pokemon","magenta"))
     println(changeCouleur("Pikachu","jaune"))
     println("pokemon ce nomme ${changeCouleur("Pikachu", "vert")} incroyable")
+    println(changeCouleur("Pikachu","marron"))
+    println(changeCouleur("Pikachu","noir"))
+    println(changeCouleur("dragon de feu ","rose"))
 
 }
 fun changeCouleur(message: String, couleur:String=""): String {
@@ -15,12 +18,15 @@ fun changeCouleur(message: String, couleur:String=""): String {
     val codeCouleur = when (couleur.lowercase()) {
         "rouge" -> "\u001B[31m"
         "vert" -> "\u001B[32m"
-        "jaune" -> "\u001B[33m"
+        "jaune" -> "\u001B[33m"            // Pour les couleur chatgpt
         "bleu" -> "\u001B[34m"
         "magenta" -> "\u001B[35m"
         "cyan" -> "\u001B[36m"
         "blanc" -> "\u001B[37m"
-        else -> "" // pas de couleur si non reconnu
+        "marron" -> "\u001B[38;5;94m"
+        "rose" -> "\u001B[38;5;213m"
+
+        else -> " impossible " // pas de couleur si non reconnu
     }
     return "$codeCouleur$message$reset"
 }
