adce0bae174fa5593d969b5719ec4532f44cbff0
josue <josuekialengela@gmail.com>
2025-09-18 09:16:06 +0200

    ini

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package org.example
 
 fun main() {
+    println(changeCouleur("Hello","rouge"))
+    println(changeCouleur("World","bleu"))
+    println("Hello ${changeCouleur("my","jaune")} World")
+    println(changeCouleur("Truc","marron"))
+    println(changeCouleur("Pokemon","magenta"))
+    println(changeCouleur("Pikachu","jaune"))
+    println("pokemon ce nomme ${changeCouleur("Pikachu", "vert")} incroyable")
 
+}
+fun changeCouleur(message: String, couleur:String=""): String {
+    val reset = "\u001B[0m"
+    val codeCouleur = when (couleur.lowercase()) {
+        "rouge" -> "\u001B[31m"
+        "vert" -> "\u001B[32m"
+        "jaune" -> "\u001B[33m"
+        "bleu" -> "\u001B[34m"
+        "magenta" -> "\u001B[35m"
+        "cyan" -> "\u001B[36m"
+        "blanc" -> "\u001B[37m"
+        else -> "" // pas de couleur si non reconnu
+    }
+    return "$codeCouleur$message$reset"
 }
